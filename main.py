a881fe30637a6652f99e79e2e906478281630fa4
One1of <ilar5806@gmail.com>
2024-03-06 21:52:31 +0300

    Comprehension of lists, dictionaries and sets

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-for x in range(5,11,2): # (начало,до какого момента, шаг)
-    print(x)
-print(range(10)) # в виде range
-print(list(range(10))) # отображается в виде списка
+# List comprehension(понимание списков)
+greeting = 'World!'
+letter_list = []
+for letter in greeting:
+    letter_list.append(letter)
+print(letter_list)
 
-str1 = 'abcdefg'
-for item in enumerate(str1): # произведёт нумерацию символов (1, 'b')
-    print(item)
+letter_list1 = [letter for letter in greeting]
+print(letter_list1)
+number_list = [number for number in '123456789'] # тут вывод будет в виде строки
+print(number_list)
+number_list1 = [number for number in range(0, 10)]
+print(number_list1)
+number_list2 = [number ** 2 for number in range(0, 10)] # как я понимаю сначала создаётся переменная(с которой уже можно как-то взаимодействовать)
+print(number_list2) # которая потом исп в цикле и мы в неё засовываем сгенерированные числа
 
-for index, letter in enumerate(str1): # произведёт нумерацию символов (1, 'b')
-    print(letter + ' is at index ' + str(index))
+number_list3 = [134, 5, -431, 700]
+new_list = [num for num in number_list3 if num > 0] # помещаем num в наш список из последовательности пред списка если этот элемент положительный
+print(new_list)
+new_list1 = ['+' if num > 0 else '-' for num in number_list3] # с else пишется вначале
+print(new_list1)
 
-print('a' in 'abc') # проверяет если такое элемент как a в abc и возвращает результат True
-print('b' in 'cde')
+# Dictionary comprehension
+num_dict = {'key': 1, 'key2': 2, 'key3': 3}
+new_dict = {key: value ** 4 for key, value in num_dict.items()}
+print(new_dict)
 
-list1 = ['a','b','c',1,'e']
-print('c' in list1)
-print(1 in list1)
+number_list4 = [134, 5, -431, 700, 0]
+num_dict1 = {number: number ** 2 for number in number_list4} # ключом является пред знач
+print(num_dict1)
+num_dict1 = {number: '+' if number > 0 else '-' if number < 0 else 'zero' for number in number_list4}
+print(num_dict1)
 
-dict1 = {1: 'a', 2:'b', 3:'c'}
-print(1 in dict1)
-print(1 in dict1.keys())
-print('b' in dict1.values())
-
-print(min(100,1,50))
-print(max(100,1,50))
-
-from random import shuffle # подключил библиотеку и импортировал команду
-list2 = [64,0,15,5]
-shuffle(list2) # перетасовывает значения
-print(list2)
-
-from random import randint # рандомное целое число
-print(randint(1,100))
+# Sets comprehension
+number_list = [134, 5, -431, 700, 0]
+num_set = {number ** 2 for number in number_list4} # множества это не упорядоченная последовательность
+print(num_set)
